268ef4ac32ee48ce008a2a1b480364d338b5a61b
Radifan <amaliradifan9a@gmail.com>
2024-01-21 01:01:42 +0700

    memperbaiki navbar user

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,45 +10,72 @@
             </ul>
         </div>
     </div>
-    <div class="flex-none me-14">
-        <div class="dropdown dropdown-end">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                <div class="indicator">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                    <span class="badge badge-sm indicator-item">8</span>
+    @auth
+        <div class="flex-none me-14">
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <div class="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        </svg>
+                        @if (auth()->user()->carts->count())
+                            <span class="badge badge-sm indicator-item">{{ auth()->user()->carts->count() }}</span>
+                        @endif
+                    </div>
                 </div>
-            </div>
-            <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-                <div class="card-body">
-                    <span class="font-bold text-lg">8 Items</span>
-                    <span class="text-info">Subtotal: $999</span>
-                    <div class="card-actions">
-                        <button class="btn btn-primary btn-block">View cart</button>
+                <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                    <div class="card-body">
+                        <span class="font-bold text-lg">{{ auth()->user()->carts->count() }} Items</span>
+                        <span class="text-info">Subtotal: Rp{{ auth()->user()->carts->sum('total_costs') }}</span>
+                        <div class="card-actions">
+                            <button class="btn btn-warning bg-warning btn-block">View cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="dropdown dropdown-end">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-                <div class="w-10 rounded-full">
-                    <img alt="Tailwind CSS Navbar component"
-                        src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img alt="Tailwind CSS Navbar component"
+                            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
                 </div>
+                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <li>
+                        <p class="justify-between text-warning">
+                            {{ auth()->user()->name }}
+                        </p>
+                    </li>
+                    <li><a href="/profile">Profile</a></li>
+                    <li>
+                        <button onclick="logout_modal.showModal()">Sign Out</button>
+                    </li>
+                </ul>
             </div>
-            <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li>
-                    <a class="justify-between">
-                        Profile
-                        <span class="badge">New</span>
-                    </a>
-                </li>
-                <li><a>Settings</a></li>
-                <li><a>Logout</a></li>
-            </ul>
+        </div>
+    @else
+        <a href="/signin"
+            class="text-base me-14 btn btn-outline border-warning
+        hover:bg-warning hover:border-warning text-warning">
+            Sign In
+        </a>
+    @endauth
+</div>
+
+<dialog id="logout_modal" class="modal modal-bottom sm:modal-middle">
+    <div class="modal-box">
+        <h3 class="font-bold text-lg">Sign Out!</h3>
+        <p class="py-4">Are You Sure Want To Sign Out?</p>
+        <div class="modal-action">
+            <form action="/logout" method="POST">
+                @csrf
+                <button class="btn btn-error">Sign Out</button>
+            </form>
+            <form method="dialog">
+                <button class="btn">Close</button>
+            </form>
         </div>
     </div>
-</div>
+</dialog>
