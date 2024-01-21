6f169eb9dc3430aef7640553fb224d0ac059eb48
Radifan <amaliradifan9a@gmail.com>
2024-01-21 15:51:30 +0700

    penyempurnaan fitur sign in dan sign up

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -7,14 +7,17 @@ use Illuminate\Http\Request;
 
 class SignupController extends Controller
 {
-    public function store(Request $request){
-        $validateData = $request-> validate([
-            'name'=>'required|min:3|max:50',
-            'email'=>'required|min:5|max:30',
-            'password'=>'required|min:5|max:30']);
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|min:5|max:30',
+            'password' => 'required|min:5|max:30'
+        ]);
 
-            User::create($validateData);
+        User::create($validateData);
+        $user = $request->input('name');
 
-        return redirect('/signin');
+        return redirect('/signin')->with('signupSuccess', "Hellooo <b>$user</b>!! You Are Successfully Registered!");
     }
 }
